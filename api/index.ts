a7c1f3bab249b1713ac7dd7d623e2d8ef61f695e
Nico Castro <nicollecastrog@gmail.com>
2019-05-07 14:40:04 +0100

    add some linting :sparkles:

--- api/index.ts
+++ api/index.ts
@@ -4,13 +4,14 @@ const typeDefs = `
   type Query {
     hello(name: String): String!
   }
-`
+`;
 
 const resolvers = {
   Query: {
-    hello: (_: void, { name }: { name: string }) => `Hello ${name || "World"}, welcome to Milo.Dog`,
-  },
-}
+    hello: (_: void, { name }: { name: string }) =>
+      `Hello ${name || "World"}, welcome to Milo.Dog`
+  }
+};
 
 const server = new GraphQLServer({ typeDefs, resolvers });
 server.start(() => console.log("Server is running on localhost:4000"));
