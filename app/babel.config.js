35d295deb72976f7f29d94ccfc207f4bcded0e17
nicollecastrog <nicollecastrog@gmail.com>
2020-05-19 21:19:05 +0100

    linting for app :sparkles:

--- app/babel.config.js
+++ app/babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   presets: [
-    'module:metro-react-native-babel-preset', // applies @babel/plugin-transform-typescript to .ts or .tsx files
-  ],
+    "module:metro-react-native-babel-preset" // applies @babel/plugin-transform-typescript to .ts or .tsx files
+  ]
 };
